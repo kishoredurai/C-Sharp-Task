243990d9ec696acdda41fbc10fefba5f5178e8e7
Kishore D <kishoredurai7@gmail.com>
2023-03-31 16:01:39 +0530

    Time calculation for new licence

---
+++ C#-Tasks/LicenceProcessTimeCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Tasks
+{
+    internal class LicenceProcessTimeCalculator
+    {
+        public static int LicenceCalculateTime(string me, int agents, string others)
+        {
+            string[] otherNames = (me + " " + others).Split(' ');
+
+            Array.Sort(otherNames);
+
+            int myIndex = Array.IndexOf(otherNames, me);
+
+            int totalTime = 20 * (myIndex / agents + 1);
+
+            return totalTime;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(LicenceCalculateTime("Eric", 2, "Adam Caroline Rebecca Frank")); // 40
+            Console.WriteLine(LicenceCalculateTime("Zebediah", 1, "Bob Jim Becky Pat")); // 100
+            Console.WriteLine(LicenceCalculateTime("Aaron", 3, "Jane Max Olivia Sam")); // 20
+        }
+    }
+}
--- C#-Tasks/LicensePlateFromat.cs
+++ C#-Tasks/LicensePlateFromat.cs
@@ -8,7 +8,7 @@ namespace C__Tasks
 {
     internal class LicensePlateFromat
     {
-        public static string LicensePlaceFormatter(string s, int n)
+        public static string LicensePlateFormatter(string s, int n)
         {
 
             string cleaned = new string(s.ToUpper().Replace("-", "").ToCharArray());  // remove all - and convert to uppercanse
@@ -22,12 +22,12 @@ namespace C__Tasks
 
             return cleaned;
         }
-        public static void Main(string[] args) 
+        public static void Mains(string[] args) 
         {
-            Console.WriteLine(LicensePlaceFormatter("5F3Z-2e-9-w", 4)); // "5F3Z-2E9W"
-            Console.WriteLine(LicensePlaceFormatter("2-5g-3-J", 2)); // "2-5G-3J"
-            Console.WriteLine(LicensePlaceFormatter("2-4A0r7-4k", 3)); // "24-A0R-74K"
-            Console.WriteLine(LicensePlaceFormatter("nlj-206-fv", 3)); // "NL-J20-6FV"
+            Console.WriteLine(LicensePlateFormatter("5F3Z-2e-9-w", 4)); // "5F3Z-2E9W"
+            Console.WriteLine(LicensePlateFormatter("2-5g-3-J", 2)); // "2-5G-3J"
+            Console.WriteLine(LicensePlateFormatter("2-4A0r7-4k", 3)); // "24-A0R-74K"
+            Console.WriteLine(LicensePlateFormatter("nlj-206-fv", 3)); // "NL-J20-6FV"
 
         }
     }
